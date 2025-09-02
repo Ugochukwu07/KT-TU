8805685674563468c7688498fddb15dbaaf6d3bd
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-02 12:31:25 +0100

    Initial commit

---
+++ src/UserInput.kt
@@ -0,0 +1,43 @@
+import kotlin.random.Random
+
+fun main (args: Array<String>){
+    //PRACTICE
+//    println("Hello, what is your name?")
+//    val userName = readLine()?:""
+//    println("Hello, $userName! Welcome to Kotlin programming.")
+
+//    println("Enter a number:")
+//    val userInput = readLine()?:""
+//    val userNumber = userInput.toIntOrNull() ?: 0
+//    println("Your number multiplied by 5 is ${userNumber * 5}")
+
+    println("What is your birth year?")
+    val userInput = readLine()?:""
+    val birthYear = userInput.toIntOrNull() ?: 0
+    val currentYear = 2025
+    val age = currentYear - birthYear
+    println("You are $age years old.")
+
+    // RANDOM NUMBER
+//    println(Random.nextInt())
+//    println(Random.nextInt(100))
+//    println(Random.nextInt(20, 30))
+//
+//    println(Random.nextDouble())
+
+//    println("Input Number to Double:")
+//    val userInput = readLine()?:""
+//
+//    println("Your input is ${userInput.toInt() * 2}")
+
+//    println("Input your name:")
+//    val userInput = readLine()?:"Nobody"
+//    val userInputInt = userInput.toIntOrNull() ?: 0
+//    println(userInputInt)
+//    println("Your name has ${userInput.length} characters")
+//    println("Your name reversed is ${userInput.reversed()}")
+//    println("The first character of your name is ${if (userInput.isNotEmpty()) userInput[0] else ' '}")
+//    println("The last character of your name is ${if (userInput.isNotEmpty()) userInput[userInput.length - 1] else ' '}")
+//    println("The name in uppercase is ${userInput.uppercase()}")
+//    println("Hello, $userInput!")
+}
