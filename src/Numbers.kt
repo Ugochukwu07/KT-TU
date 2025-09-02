e65eb7008222d1453684fbe64ab034f31a12ad12
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-02 15:11:43 +0100

    add initial implementation of user input and random number operations
    Signed-off-by: Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>

---
+++ src/Numbers.kt
@@ -0,0 +1,26 @@
+fun main (args: Array<String>){
+
+    println("Please enter a number:")
+
+
+//    var price = 34;
+//    var itmes = 5;
+//    var total_price = price * itmes;
+//    println("Total price is $total_price dollars")
+
+//    var items = 8
+//    println(items::class.java)
+//
+//    var number_of_people = 7500000000
+//    println(number_of_people::class.java)
+//
+//    var price: Float = 29.99F
+//    println(price::class.java)
+//
+//    var pi = 3.14159265358979323846
+//    println(pi::class.java)
+//
+//    var letter = 'A'
+//    println(letter::class.java)
+
+}
--- src/UserInput.kt
+++ src/UserInput.kt
@@ -1,43 +1,53 @@
 import kotlin.random.Random
 
 fun main (args: Array<String>){
+    //Implicit and Explicit Types
+    var dog: Byte = 5
+    println(dog::class.java)
+
     //PRACTICE
-//    println("Hello, what is your name?")
-//    val userName = readLine()?:""
-//    println("Hello, $userName! Welcome to Kotlin programming.")
+    println("Hello, what is your name?")
+    val userName = readLine()?:""
+    println("Hello, $userName! Welcome to Kotlin programming.")
 
-//    println("Enter a number:")
-//    val userInput = readLine()?:""
-//    val userNumber = userInput.toIntOrNull() ?: 0
-//    println("Your number multiplied by 5 is ${userNumber * 5}")
+    println("Enter a number:")
+    val userInput1 = readlnOrNull() ?:""
+    val userNumber1 = userInput1.toInt()
+    val randomFloat = Random.nextDouble(14.99, 500.00)
+    val outcome = userNumber1 * randomFloat
+    println("Your number multiplied by a random float ($randomFloat) is $outcome and type is ${outcome::class.java}")
 
-//    println("What is your birth year?")
-//    val userInput = readLine()?:""
-//    val birthYear = userInput.toIntOrNull() ?: 0
-//    val currentYear = 2025
-//    val age = currentYear - birthYear
-//    println("You are $age years old.")
+    println("Enter a number:")
+    val userInput2 = readLine()?:""
+    val userNumber = userInput2.toIntOrNull() ?: 0
+    println("Your number multiplied by 5 is ${userNumber * 5}")
+
+    println("What is your birth year?")
+    val userInput3 = readLine()?:""
+    val birthYear = userInput3.toIntOrNull() ?: 0
+    val currentYear = 2025
+    val age = currentYear - birthYear
+    println("You are $age years old.")
 
     // RANDOM NUMBER
-//    println(Random.nextInt())
-//    println(Random.nextInt(100))
-//    println(Random.nextInt(20, 30))
-//
-//    println(Random.nextDouble())
+    println(Random.nextInt())
+    println(Random.nextInt(100))
+    println(Random.nextInt(20, 30))
 
-//    println("Input Number to Double:")
-//    val userInput = readLine()?:""
-//
-//    println("Your input is ${userInput.toInt() * 2}")
+    println(Random.nextDouble())
 
-//    println("Input your name:")
-//    val userInput = readLine()?:"Nobody"
-//    val userInputInt = userInput.toIntOrNull() ?: 0
-//    println(userInputInt)
-//    println("Your name has ${userInput.length} characters")
-//    println("Your name reversed is ${userInput.reversed()}")
-//    println("The first character of your name is ${if (userInput.isNotEmpty()) userInput[0] else ' '}")
-//    println("The last character of your name is ${if (userInput.isNotEmpty()) userInput[userInput.length - 1] else ' '}")
-//    println("The name in uppercase is ${userInput.uppercase()}")
-//    println("Hello, $userInput!")
+    println("Input Number to Double:")
+    val userInput4 = readLine()?:""
+    println("Your input is ${userInput4.toInt() * 2}")
+
+    println("Input your name:")
+    val userInput = readLine()?:"Nobody"
+    val userInputInt = userInput.toIntOrNull() ?: 0
+    println(userInputInt)
+    println("Your name has ${userInput.length} characters")
+    println("Your name reversed is ${userInput.reversed()}")
+    println("The first character of your name is ${if (userInput.isNotEmpty()) userInput[0] else ' '}")
+    println("The last character of your name is ${if (userInput.isNotEmpty()) userInput[userInput.length - 1] else ' '}")
+    println("The name in uppercase is ${userInput.uppercase()}")
+    println("Hello, $userInput!")
 }
