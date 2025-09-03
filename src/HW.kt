30cce9120a6e944d3d048b531f492d1350e10c35
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-03 10:45:29 +0100

    uncomment code examples in HW.kt and Numbers.kt for better clarity and demonstration
    Signed-off-by: Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>

--- src/HW.kt
+++ src/HW.kt
@@ -7,57 +7,57 @@ fun main (args: Array<String>){
 
     println("Accessing url $URL_LINK...")
 
-//    var dogName = "Teddy";
-//    println("My dog name is $dogName");
-//    println("I have ${1+1} cats")
-//
-//    var apples = 4
-//    var mango = 6
-//
-//    println("I have $apples apples and $mango mangoes, a total of ${apples + mango} fruits")
-//
-//    val client = "Mary"
-//    val products = 6
-//    val price = 30
-//
-//    println("$client bought $products items, spent $price dollars")
+    var dogName = "Teddy";
+    println("My dog name is $dogName");
+    println("I have ${1+1} cats")
+
+    var apples = 4
+    var mango = 6
+
+    println("I have $apples apples and $mango mangoes, a total of ${apples + mango} fruits")
+
+    val client = "Mary"
+    val products = 6
+    val price = 30
+
+    println("$client bought $products items, spent $price dollars")
 
 
-//    var tvShows = "many"
-//    println(tvShows)
-//    tvShows = "too many"
-//    println(tvShows)
+    var tvShows = "many"
+    println(tvShows)
+    tvShows = "too many"
+    println(tvShows)
 
 
 //    var age: String = 25
 //    age = "many"
 
-//    val color = "blue"
+    val color = "blue"
 //    color = "red"
 
-//    val catName = "My cat's name is \"Fluffy\""
-//    println(catName)
+    val catName = "My cat's name is \"Fluffy\""
+    println(catName)
 
-//    var slashes = "Two types of slashes: \\ and /"
-//    println(slashes)
-//
-//    val google = "https://google.com"
-//    println(google)
-//
-//    val ears = "(\\(\\"
-//    val head = "(-.-)"
-//    val body = "o_(\")(\")"
-//    println(ears)
-//    println(head)
-//    println(body)
-//
-//    val myPet = "Crocodiel"
-//    println(myPet.length)
-//    println(myPet.uppercase(getDefault()))
-//    println(myPet.get(3))
-//    println(myPet.indexOf("c"))
-//    println(myPet[3]);
-//    println(myPet.substring(3))
-//    println(myPet.substring(3, 5))
+    var slashes = "Two types of slashes: \\ and /"
+    println(slashes)
+
+    val google = "https://google.com"
+    println(google)
+
+    val ears = "(\\(\\"
+    val head = "(-.-)"
+    val body = "o_(\")(\")"
+    println(ears)
+    println(head)
+    println(body)
+
+    val myPet = "Crocodiel"
+    println(myPet.length)
+    println(myPet.uppercase(getDefault()))
+    println(myPet.get(3))
+    println(myPet.indexOf("c"))
+    println(myPet[3]);
+    println(myPet.substring(3))
+    println(myPet.substring(3, 5))
     
 }
