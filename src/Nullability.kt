2c63596b0f2c545ea3986c206cc836ae10741b10
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-03 09:47:18 +0100

    refactor Nullability.kt to improve null handling and enhance user input examples
    Signed-off-by: Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>

--- src/Nullability.kt
+++ src/Nullability.kt
@@ -1,32 +1,37 @@
 fun main(args: Array<String>) {
 
+    var table: String? = null
+    println(table!!.length)
+
+    var catName: String? = null
+    catName = "Fluffy"
+    println(catName?:"This cat has no name")
+
     println("Enter a number:")
     var userInput = readLine()
-
     var multi = userInput?.toDouble()?.times(7)
-
     println(multi?.toString())
 
 
-//var cow: String? = "Fluffy The Cow and Friends"
-//println(cow?.substring(3,7))
+    var cow: String? = "Fluffy The Cow and Friends"
+    println(cow?.substring(3,7))
 
-//    var a: Int? = 10
-//
-//    println(a)
-//    println(a?.plus(3))
-//    println(a?.minus(3))
-//    println(a?.times(3))
-//    println(a?.div(3))
-//    println(a?.rem(3))
+    var a: Int? = 10
 
-//   var herName: String = "Sally"
-//    var hisName: String? = null
-//
-//    println(herName)
-//    println(hisName)
-//
-//    herName = null
-//
+    println(a)
+    println(a?.plus(3))
+    println(a?.minus(3))
+    println(a?.times(3))
+    println(a?.div(3))
+    println(a?.rem(3))
+
+    var herName: String = "Sally"
+    var hisName: String? = null
+
+    println(herName)
+    println(hisName)
+
+//    herName: String? = null
+
 //    println(herName)
 }
