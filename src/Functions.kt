a891cafada560c51430dd1e15f879385dc07c41e
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-08 09:58:09 +0100

    refactor Functions.kt to enhance functionality with doubleMessage and sayHelloToPersons; remove commented-out code
    Signed-off-by: Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>

--- src/Functions.kt
+++ src/Functions.kt
@@ -1,11 +1,25 @@
 fun main (args: Array<String>){
-    for(i in 1..3){
-        sayHello()
-    }
+//    for(i in 1..3){
+//        sayHello()
+//    }
+//
+//    for(j in 1..2){
+//        multiplyNumber()
+//    }
 
-    for(j in 1..2){
-        multiplyNumber()
-    }
+//    for (k in 1..3){
+//        double(k)
+//    }
+
+//    val persons = listOf("Alice", "Bob", "Charlie")
+//    sayHelloToPersons(persons)
+
+    doubleMessage(5)
+    doubleMessage(10, "The double value is ")
+}
+
+fun doubleMessage(number: Int, message: String = "Double is ") {
+    println("$message ${number * 2}")
 }
 
 fun sayHello(){
@@ -17,4 +31,14 @@ fun multiplyNumber(){
     val input = readLine()!!.toInt()
     val result = input * 17
     println("The result is $result")
+}
+
+fun double(x: Int) {
+    println("The double of $x is ${x * 2}")
+}
+
+fun sayHelloToPersons(persons: Collection<String>){
+    for(person in persons){
+        println("Hello $person")
+    }
 }
