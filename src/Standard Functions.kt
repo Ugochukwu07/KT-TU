e7f490466a8f3bc327a3e96934f26d9e1c367cf0
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-10 16:16:50 +0100

    add STCar class with speed control and main function for car operations
    Signed-off-by: Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>

--- src/Standard Functions.kt
+++ src/Standard Functions.kt
@@ -1,9 +1,48 @@
 fun main (args: Array<String>) {
-    val animals: List<String> = listOf("dog", "cat", "elephant", "tiger", "lion", "rat", "girrafe")
-    animals.map { it.length }
-        .filter { it > 3 }
-        .let{
-            println(it)
-            println("Total items: ${it.size}")
+//    val animals: List<String> = listOf("dog", "cat", "elephant", "tiger", "lion", "rat", "girrafe")
+//    animals.map { it.length }
+//        .filter { it > 3 }
+//        .let{ list: List<Int> -> println(list) }
+//        .let{ filteredList ->
+//            println(filteredList)
+//            println("Total items: ${filteredList.size}")
+//        }
+
+    with(STCar()){
+        speed = 30
+        accelerate()
+        println("Current speed: $speed")
+        applyBrakes()
+        applyBrakes(2)
+        println("Current speed: $speed")
+    }
+}
+
+class STCar {
+    var speed: Int = 0
+        set(value){
+            //make sure speed is never negative
+            if(value < 0){
+                field = 0
+                return
+            }
+            field = value
         }
+    var color: String = "Red"
+    var model: String = "BMW"
+
+    fun applyBrakes() {
+        println("Applying brakes")
+        this.speed -= 10
+    }
+
+    fun accelerate() {
+        println("Accelerating")
+        this.speed += 10
+    }
+
+    fun applyBrakes(decrement: Int) {
+        println("Applying brakes with decrement: $decrement")
+        this.speed -= decrement
+    }
 }
