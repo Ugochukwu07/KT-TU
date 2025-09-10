9484207b50c5b0e3264393bc8b9b5373df98a073
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-10 10:38:37 +0100

    refactor Construtors.kt to uncomment and organize MyCarBag class constructors and improve main function usage
    Signed-off-by: Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>

--- src/Construtors.kt
+++ src/Construtors.kt
@@ -1,38 +1,38 @@
 fun main (args: Array<String>){
-//    var car = MyCarBag("Toyota", 180)
-//    var car2 = MyCarBag("Honda")
+    var car = MyCarBag("Toyota", 180)
+    var car2 = MyCarBag("Honda")
 
-//    var car3 = MyCarBag()
-//    car3.topSpeed = 200
-//    println(car3.model)
-//    println(car3.topSpeed)
+    var car3 = MyCarBag()
+    car3.topSpeed = 200
+    println(car3.model)
+    println(car3.topSpeed)
 
     var user = User(1.1, 300.0)
 //    user.
-//    println(user.intrestRate)
-//    println(user.balance)
+    println(user.intrestRate)
+    println(user.balance)
 
 }
 
 class MyCarBag{
 
-//    constructor(){
-//        model = "Unknown"
-//        topSpeed = 150
-//        println("Car $model with top speed of $topSpeed km/h has been created.")
-//    }
-//
-//    constructor(newModel: String){
-//        model = newModel
-//        topSpeed = 150
-//        println("Car $model with top speed of $topSpeed km/h has been created.")
-//    }
-//
-//    constructor(newModel: String, newTopSpeed: Int){
-//        model = newModel
-//        topSpeed = newTopSpeed
-//        println("Car $model with top speed of $topSpeed km/h has been created.")
-//    }
+    constructor(){
+        model = "Unknown"
+        topSpeed = 150
+        println("Car $model with top speed of $topSpeed km/h has been created.")
+    }
+
+    constructor(newModel: String){
+        model = newModel
+        topSpeed = 150
+        println("Car $model with top speed of $topSpeed km/h has been created.")
+    }
+
+    constructor(newModel: String, newTopSpeed: Int){
+        model = newModel
+        topSpeed = newTopSpeed
+        println("Car $model with top speed of $topSpeed km/h has been created.")
+    }
 
     var model: String? = null
     var topSpeed: Int
@@ -42,24 +42,24 @@ class MyCarBag{
         }
 }
 
-//class MyCarBag(var model: String, var topSpeed: Int){
-//
-//    init {
-//        println("Car $model with top speed of $topSpeed km/h has been created.")
-//    }
-//
-//    fun getModel(){
-//        println("The model is $model")
-//    }
-//
-//    fun start(){
-//        println("The car is starting")
-//    }
-//
-//    fun drive(speed: Int){
-//        println("Driving at $speed km/h")
-//    }
-//}
+class MyCarBag2(var model: String, var topSpeed: Int){
+
+    init {
+        println("Car $model with top speed of $topSpeed km/h has been created.")
+    }
+
+    fun getModel(){
+        println("The model is $model")
+    }
+
+    fun start(){
+        println("The car is starting")
+    }
+
+    fun drive(speed: Int){
+        println("Driving at $speed km/h")
+    }
+}
 
 class User{
     var intrestRate = 0.0
