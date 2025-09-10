686862be9472000d2275f21d9e651c5a273c116b
Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>
2025-09-10 10:05:04 +0100

    refactor Construtors.kt to update MyCarBag and User classes with new properties and constructors
    Signed-off-by: Ugochukwu Ekwueme <ekwuemeugochukwu83@gmail.com>

--- src/Construtors.kt
+++ src/Construtors.kt
@@ -1,32 +1,45 @@
 fun main (args: Array<String>){
-    var car = MyCarBag("Toyota", 180)
-    var car2 = MyCarBag("Honda")
-    var car3 = MyCarBag()
+//    var car = MyCarBag("Toyota", 180)
+//    var car2 = MyCarBag("Honda")
+
+//    var car3 = MyCarBag()
+//    car3.topSpeed = 200
+//    println(car3.model)
+//    println(car3.topSpeed)
+
+    var user = User(1.1, 300.0)
+//    user.
+//    println(user.intrestRate)
+//    println(user.balance)
 
 }
 
 class MyCarBag{
 
-    constructor(){
-        model = "Unknown"
-        topSpeed = 150
-        println("Car $model with top speed of $topSpeed km/h has been created.")
-    }
-
-    constructor(newModel: String){
-        model = newModel
-        topSpeed = 150
-        println("Car $model with top speed of $topSpeed km/h has been created.")
-    }
-
-    constructor(newModel: String, newTopSpeed: Int){
-        model = newModel
-        topSpeed = newTopSpeed
-        println("Car $model with top speed of $topSpeed km/h has been created.")
-    }
+//    constructor(){
+//        model = "Unknown"
+//        topSpeed = 150
+//        println("Car $model with top speed of $topSpeed km/h has been created.")
+//    }
+//
+//    constructor(newModel: String){
+//        model = newModel
+//        topSpeed = 150
+//        println("Car $model with top speed of $topSpeed km/h has been created.")
+//    }
+//
+//    constructor(newModel: String, newTopSpeed: Int){
+//        model = newModel
+//        topSpeed = newTopSpeed
+//        println("Car $model with top speed of $topSpeed km/h has been created.")
+//    }
 
     var model: String? = null
-    var topSpeed: Int = 100
+    var topSpeed: Int
+        get() = 10
+        set(value){
+            model = "Unknown"
+        }
 }
 
 //class MyCarBag(var model: String, var topSpeed: Int){
@@ -47,3 +60,30 @@ class MyCarBag{
 //        println("Driving at $speed km/h")
 //    }
 //}
+
+class User{
+    var intrestRate = 0.0
+    var balance = 0.0
+        get() = field
+        public set(value){
+            if (value < 10)
+                intrestRate = 0.5
+            else if (value < 1000)
+                intrestRate = 1.0
+            else if (value > 10000)
+                intrestRate = 0.2
+            field = value
+        }
+
+    constructor(intrestRate: Double, balance: Double){
+        this.intrestRate += intrestRate
+        this.balance = balance
+
+        println("User with balance $balance and intrest rate $intrestRate has been created.")
+    }
+
+    init {
+        intrestRate = 200.0
+        println("New user created.")
+    }
+}
